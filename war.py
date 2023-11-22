864903efcb6e57ddcedafcd05ad1af324d01be98
Inokus <inokus@outlook.com>
2023-11-22 18:43:14 +0200

    Add war.py

---
+++ war.py
@@ -0,0 +1,133 @@
+import random
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
+
+class Deck:
+    suits = ("♣", "♦", "♥", "♠")
+    ranks = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
+    deck = []
+
+    for suit in suits:
+        for rank in ranks:
+            deck.append(Card(suit, rank))
+
+    # Might not need this
+    @classmethod
+    def get_deck(cls):
+        return cls.deck
+
+    @classmethod
+    def shuffle(cls):
+        random.shuffle(cls.deck)
+
+    @classmethod
+    def split_in_half(cls):
+        split_point = len(cls.deck) // 2
+        half1 = cls.deck[:split_point]
+        half2 = cls.deck[split_point:]
+        return half1, half2
+
+
+class Player:
+    def __init__(self, name, cards):
+        self.name = name
+        self.cards = cards
+
+
+class Game:
+    def __init__(self, player1, player2):
+        self.card_values = {
+            "2": 0,
+            "3": 1,
+            "4": 2,
+            "5": 3,
+            "6": 4,
+            "7": 5,
+            "8": 6,
+            "9": 7,
+            "10": 8,
+            "J": 9,
+            "Q": 10,
+            "K": 11,
+            "A": 12,
+        }
+
+        self.player1 = player1
+        self.player2 = player2
+        self.player1_card_count = len(self.player1.cards)
+        self.player2_card_count = len(self.player2.cards)
+
+    def compare_cards(self, card1, card2):
+        print(f"{card1} vs {card2}")
+
+        if self.card_values[card1.rank] > self.card_values[card2.rank]:
+            return 1
+        elif self.card_values[card1.rank] < self.card_values[card2.rank]:
+            return -1
+        else:
+            return 0
+
+    # Should we shuffle cards? Whose cards goes first if not?
+    def take_cards(self, winning_player, losing_player, index):
+        winning_player.cards = (
+            winning_player.cards[index + 1 :]
+            + winning_player.cards[0 : index + 1]
+            + losing_player.cards[0 : index + 1]
+        )
+        losing_player.cards = losing_player.cards[index + 1 :]
+
+    def play(self):
+        while self.player1_card_count > 0 and self.player2_card_count > 0:
+            current_index = 0
+            while True:
+                comparison_evaluation = self.compare_cards(
+                    self.player1.cards[current_index], self.player2.cards[current_index]
+                )
+                # Add and subtract from player_card_counts
+                if comparison_evaluation == 1:
+                    self.take_cards(self.player1, self.player2, current_index)
+                    self.player1_card_count += current_index + 1
+                    self.player2_card_count -= current_index + 1
+                    break
+                elif comparison_evaluation == -1:
+                    self.take_cards(self.player2, self.player1, current_index)
+                    self.player2_card_count += current_index + 1
+                    self.player1_card_count -= current_index + 1
+                    break
+                else:
+                    # Need to check if both sides has enough cards left to go to war
+                    current_index += 2
+                    if self.player1_card_count < current_index + 1:
+                        return f"{self.player2.name} wins!"
+                    elif self.player2_card_count < current_index + 1:
+                        return f"{self.player1.name} wins!"
+
+        if self.player1_card_count == 0:
+            return f"{self.player2.name} wins!"
+        else:
+            return f"{self.player1.name} wins!"
+
+
+def main():
+    # Initialize the deck and shuffle it
+    deck = Deck()
+    deck.shuffle()
+
+    # Split shuffled deck in two and assign cards to both players
+    half1, half2 = deck.split_in_half()
+    player1 = Player("Josh", half1)
+    player2 = Player("Alice", half2)
+
+    game = Game(player1, player2)
+    print(game.play())
+
+
+if __name__ == "__main__":
+    main()
